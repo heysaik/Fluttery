b516c092a6efe7de2445f042644be8528c1d251c
Sai Kambampati <saikambampati@Goliath.attlocal.net>
2020-05-28 22:43:36 -0700

    Initial Commit

---
+++ 02 Friendly Chat/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: 'FriendlyChat',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("FriendlyChat") 
+        )
+      ),
+    )
+  );
+}
